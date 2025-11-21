046d839ddf503ebc007948ce8deb32df23a4f076
Frank Zeng <frank.zeng.0814@gmail.com>
2025-11-21 12:00:09 +1030

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
@@ -12,6 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
+        <ClerkProvider>
         <html lang="en">
             <body className={`${outfit.className} antialiased`}>
                 <StoreProvider>
@@ -19,6 +21,7 @@ export default function RootLayout({ children }) {
                     {children}
                 </StoreProvider>
             </body>
-        </html>
+            </html>
+        </ClerkProvider>
     );
 }
